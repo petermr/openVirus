d10af0cf3d7574d4d88512027afec844316bb9c7
Clyde Davies <clyde.davies@gmail.com>
2020-04-05 09:40:07 +0100

    First commit of the splitter utility

---
+++ textIndexing/DOAJSplitter/DOAJSplitter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.CommandLine;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace DOAJSplitter
+{
+    /// <summary>
+    /// Takes a DOAJ archive file and splits into a series of
+    /// individual article files
+    /// </summary>
+    internal class Program
+    {
+        // see https://docs.microsoft.com/en-us/archive/msdn-magazine/2019/march/net-parse-the-command-line-with-system-commandline
+        private static void Main(string[] args)
+        {
+            var inputFile = args[0];
+            var outputDir = args[1];
+            if (string.IsNullOrEmpty(outputDir)) outputDir = ".";
+            var reader = new JsonTextReader(new StreamReader(inputFile));
+
+            int iCounter = 0;
+            while (reader.Read())
+                switch (reader.TokenType)
+                {
+                    case JsonToken.StartArray: //the root level object is an array specification
+                        break;
+                    case JsonToken.StartObject:
+                        var obj = JObject.Load(reader);
+                        var writer = new StreamWriter($"{outputDir}/{obj["id"]}.json");
+                        writer.Write(obj.ToString());
+                        writer.Close();
+                        Console.Write($"\rInput file '{inputFile}'; Writing file {++iCounter}      ");
+                        break;
+                }
+        }
+    }
+}
